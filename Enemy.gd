8efd290e457c023fe18aa34b01d1670d9cdc7318
Firstdragon <anden777@ya.ru>
2024-04-13 10:17:38 +0300

    fixed accuracy

---
+++ Enemy.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+@export_group("Color")
+@export var blue = Color("#4682b4")
+@export var green = Color("#639765")
+@export var red = Color("#a65455")
+
+@export_range(0,20,0.1) var speed: float
+
+
+@onready var prompt = $RichTextLabel
+@onready var prompt_text = prompt.text
+
+
+func _ready() -> void:
+	prompt_text = PromptList.get_prompt()
+	prompt.parse_bbcode(set_center_tags(prompt_text))
+	GlobalSignals.connect("difficulty_increased", Callable(self, "handle_difficulty_increased"))
+
+
+func _physics_process(_delta: float) -> void:
+	global_position.y += speed
+
+
+func set_difficulty(difficulty: int):
+	handle_difficulty_increased(difficulty)
+
+
+func handle_difficulty_increased(new_difficulty: int):
+	#var new_speed = speed + (0.125 * new_difficulty)
+	speed = 2 + (0.125 * new_difficulty)
+
+
+
+
+func get_prompt() -> String:
+	return prompt_text
+
+
+func set_next_character(next_character_index: int):
+	var blue_text = get_bbcode_color_tag(blue) + prompt_text.substr(0, next_character_index) + get_bbcode_end_color_tag()
+	var green_text = get_bbcode_color_tag(green) + prompt_text.substr(next_character_index, 1) + get_bbcode_end_color_tag()
+	var red_text = ""
+
+	if next_character_index != prompt_text.length():
+		red_text = get_bbcode_color_tag(red) + prompt_text.substr(next_character_index + 1, prompt_text.length() - next_character_index + 1) + get_bbcode_end_color_tag()
+
+	prompt.parse_bbcode(set_center_tags(blue_text + green_text + red_text))
+
+
+func set_center_tags(string_to_center: String):
+	return "[center]" + string_to_center + "[/center]"
+
+
+func get_bbcode_color_tag(color: Color) -> String:
+	return "[color=#" + color.to_html(false) + "]"
+
+
+func get_bbcode_end_color_tag() -> String:
+	return "[/color]"
---
+++ Main.gd
@@ -0,0 +1,150 @@
+extends Node2D
+
+var Enemy = preload("res://Enemy.tscn")
+
+
+@onready var enemy_container = $EnemyContainer
+@onready var spawn_container = $SpawnContainer
+@onready var spawn_timer = $SpawnTimer
+@onready var difficulty_timer = $DifficultyTimer
+
+@onready var difficulty_value = $CanvasLayer/HBoxContainer2/DifficultyValue
+@onready var killed_value = $CanvasLayer/HBoxContainer/EnemiesKilledValue
+@onready var game_over_screen = $CanvasLayer/GameOverScreen
+@onready var game_over_killed_value = $CanvasLayer/GameOverScreen/CenterContainer/VBoxContainer/HBoxContainer2/GameOverKilledValue
+@onready var accuracy_value = $CanvasLayer/GameOverScreen/CenterContainer/VBoxContainer/HBoxContainer/AccuracyValue
+
+var active_enemy = null
+var current_letter_index: int = -1
+
+var lastposx: int = 40
+var difficulty: int = 0
+var enemies_killed: int = 0
+var accuracy: float = 0
+var correctlytyped: float = 0
+var incorrectlytyped: float = 0
+var firstletters: Array
+
+
+func _ready() -> void:
+	start_game()
+
+
+func find_new_active_enemy(typed_character: String):
+	for enemy in enemy_container.get_children():
+		var prompt = enemy.get_prompt()
+		var next_character = prompt.substr(0, 1)
+		firstletters.append(next_character)
+		if next_character == typed_character or next_character == typed_character.to_upper():
+			print("found new enemy that starts with %s" % next_character.to_upper())
+			active_enemy = enemy
+			current_letter_index = 1
+			active_enemy.set_next_character(current_letter_index)
+			return
+	if typed_character != "" and firstletters.find(typed_character) == -1:
+		print("incorrectly typed %s instead of %s" % [typed_character,firstletters[0]])
+		incorrectlytyped += 1
+		print(incorrectlytyped)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
+		var typed_event = event as InputEventKey
+		var key_typed = PackedByteArray([typed_event.unicode]).get_string_from_utf8()
+
+		if active_enemy == null:
+			find_new_active_enemy(key_typed)
+		else:
+			var prompt = active_enemy.get_prompt()
+			var next_character = prompt.substr(current_letter_index, 1)
+			if key_typed == next_character:
+				print("successfully typed %s" % key_typed)
+				correctlytyped += 1
+				print(correctlytyped)
+				current_letter_index += 1
+				active_enemy.set_next_character(current_letter_index)
+				if current_letter_index == prompt.length():
+					print("done")
+					current_letter_index = -1
+					active_enemy.queue_free()
+					active_enemy = null
+					enemies_killed += 1
+					killed_value.text = str(enemies_killed)
+					if enemy_container.get_child_count() < 2:
+						spawn_enemy()
+			else:
+				print("incorrectly typed %s instead of %s" % [key_typed, next_character])
+				incorrectlytyped += 1
+				print(incorrectlytyped)
+
+func _on_SpawnTimer_timeout() -> void:
+	spawn_enemy()
+
+
+
+func spawn_enemy():
+	var enemy_instance = Enemy.instantiate()
+	#var spawns = spawn_container.get_children()
+	#var index = randi() % spawns.size()
+	var pos = global_position
+	if lastposx < 250:
+		pos.x = randi_range(lastposx + 20, 450)
+	else:
+		pos.x = randi_range(40, lastposx - 90)
+	lastposx = pos.x
+	pos.y = -20
+	enemy_instance.global_position = pos
+	enemy_container.add_child(enemy_instance)
+	enemy_instance.set_difficulty(difficulty)
+
+
+func _on_DifficultyTimer_timeout() -> void:
+	if difficulty >= 20:
+		difficulty_timer.stop()
+		difficulty = 20
+		return
+
+	difficulty += 1
+	GlobalSignals.emit_signal("difficulty_increased", difficulty)
+	print("Difficulty increased to %d" % difficulty)
+	var new_wait_time = spawn_timer.wait_time - 0.2
+	spawn_timer.wait_time = clamp(new_wait_time, 1, spawn_timer.wait_time)
+	difficulty_value.text = str(difficulty)
+
+
+func _on_LoseArea_body_entered(_body: Node) -> void:
+	game_over()
+
+
+func game_over():
+	game_over_killed_value.text = str(enemies_killed)
+	if (correctlytyped+incorrectlytyped) == 0:
+		accuracy = 0;
+	else:
+		accuracy = correctlytyped * 100 / (correctlytyped+incorrectlytyped)
+	accuracy_value.text = str(snapped(accuracy,0.001))
+	game_over_screen.show()
+	spawn_timer.stop()
+	difficulty_timer.stop()
+	active_enemy = null
+	current_letter_index = -1
+	for enemy in enemy_container.get_children():
+		enemy.queue_free()
+
+
+func start_game():
+	game_over_screen.hide()
+	difficulty = 0
+	enemies_killed = 0
+	correctlytyped = 0
+	incorrectlytyped = 0
+	difficulty_value.text = str(0)
+	killed_value.text = str(0)
+	randomize()
+	spawn_timer.start()
+	difficulty_timer.start()
+	spawn_enemy()
+
+
+func _on_RestartButton_pressed() -> void:
+	start_game()
